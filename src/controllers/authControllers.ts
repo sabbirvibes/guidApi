c71cdde422496a811e3163e3a488836cdec7396b
sabbirvibes <programingwithsabbir@gmail.com>
2025-08-19 21:34:29 +0600

    Register Functionality Created complete!

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,11 +1,43 @@
 import { Request, Response } from "express";
-import createHttpError from "http-errors";
+import { registerDto, registerValidate } from "../dtos/auth.dtos";
+import UserModel from "../models/UserModel";
 export const registerUser = async (req: Request, res: Response) => {
   try {
-    const error = createHttpError(400, "Bad Request: Invalid user data");
-    throw error;
+    const validate = registerValidate.safeParse(req.body);
+    const data: registerDto = validate.data!;
+    if (!validate.success) {
+      return res.status(400).json({
+        success: false,
+        errorType: "dtos",
+        message: validate.error.flatten().fieldErrors,
+      });
+    }
+
+    const existUser = await UserModel.findOne({
+      email: data.email,
+      username: data.username,
+    });
+
+    if (existUser) {
+      return res.status(400).json({
+        success: false,
+        errorType: "existUser",
+        message: "User Already Exist!Please Enter Valid Email Or Username",
+      });
+    }
+
+    const createUser = await UserModel.create({
+      username: data.username,
+      email: data.email,
+      password: data.password,
+      role: data.role,
+    });
     // Registration logic here
-    res.status(201).send("User registered successfully");
+    res.status(201).json({
+      success: true,
+      message: "User Register successfully",
+      data: createUser,
+    });
   } catch (err) {
     res.status(500).send(`Server error: ${err}`);
   }
